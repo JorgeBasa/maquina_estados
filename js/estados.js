c0b8ba08afd4846af103248253f291e9173743d2
JorgeBasa <jorge.basabilvazo@gmail.com>
2024-02-15 16:42:18 -0700

    Update 240215

--- js/estados.js
+++ js/estados.js
@@ -1,11 +1,34 @@
 const estado_base = {
     inicializar: () => {
-        document.querySelector("#mostrar:estado").innerText = "Está en un estado base"    },
+        document.querySelector("#mostrar:estado"),innerText = "jajaalchile";
+        document.querySelector("#boton1").innerText = "Cambiar a Llorar por Comida";
+        document.querySelector("#boton2").innerText = "Cambiar a Dormir";  },
     
-    actualizar: () => {
-        document.querySelector("#mostrar_estado")
+    actualizar: (evento) => {
+        document.querySelector("#mostrar_estado").innerText = "boton pulsao",
+        console.log(evento)
+
+        if (evento.div && evento.div.id === "boton1"){
+            maquina_estados.cambiar_estado(llorar_por_comida);
+        }
     },
 
-    finalizar: () => {},
+    finalizar: () => {
+        document.querySelector("#mostrar_estado").innerText = "Saliendo del estado"
+    },
+
+}
+
+const llorar_por_comida = {
+    inicializar: () => {
+        document.querySelector("#mostrar:estado").innerText = "Está llorando por comida"    },
+    
+    actualizar: () => {
+        //document.querySelector("#mostrar_estado").innerText = "boton pulsao"
+    },
+
+    finalizar: () => {
+        document.querySelector("#mostrar_estado").innerText = "Saliendo del estado y llorando por comida"
+    },
 
 }
